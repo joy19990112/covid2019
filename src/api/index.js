fb6b44d8ca82682d0b3fcefa791d83081da97618
joy <mahongyu19990112@163.com>
2022-11-30 15:28:13 +0800

    feat: 新增接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,50 @@
-import { get } from "../utils/request"
+import { get } from '../utils/request'
 
 const api = {
   // 国内
-    getDataInfo(params){
-        return get("http://api.tianapi.com/txapi/ncov/index",params);
-    },
-    getCaseNum(params){
-        return get("http://api.tianapi.com/txapi/ncovcity/index",params)
-    },
-    // 海外肺炎疫情
-    getAbroadInfo(params){
-      return get("http://api.tianapi.com/txapi/ncovabroad/index",params)
-  }
+  getDataInfo(
+    params
+  ) {
+    return get(
+      'http://api.tianapi.com/txapi/ncov/index',
+      params
+    )
+  },
+  getCaseNum(
+    params
+  ) {
+    return get(
+      'http://api.tianapi.com/txapi/ncovcity/index',
+      params
+    )
+  },
+  // 海外肺炎疫情
+  getAbroadInfo(
+    params
+  ) {
+    return get(
+      'http://api.tianapi.com/txapi/ncovabroad/index',
+      params
+    )
+  },
+  // 出行政策
+  getTravelPolicy(
+    params
+  ) {
+    return get(
+      'http://iwenwiki.com/wapicovid19/query.php',
+      params
+    )
+  },
+  // 城市列表清单
+  getCityInfo(
+    params
+  ) {
+    return get(
+      'http://iwenwiki.com/wapicovid19/citys.php',
+      params
+    )
+  },
 }
 
-export default api;
+export default api
